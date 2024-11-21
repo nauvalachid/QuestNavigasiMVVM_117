51393592411257a6538a12c4a8282c3d1a4a7d01
nauvalachid <nauvalachid@gmail.com>
2024-11-21 20:54:12 +0700

    Mengatur tata letak form menggunakan `Column` dengan alignment horizontal di tengah dan padding sebesar 30dp untuk memberikan ruang di sekitar elemen form.

--- app/src/main/java/com/example/tespam42/ui/view/formulirview.kt
+++ app/src/main/java/com/example/tespam42/ui/view/formulirview.kt
@@ -1,11 +1,16 @@
 package com.example.tespam42.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormulirView(modifier: Modifier = Modifier, listJK : List<String>,
@@ -17,3 +22,8 @@ fun FormulirView(modifier: Modifier = Modifier, listJK : List<String>,
     var gender by remember { mutableStateOf("") }
 
     val listData : MutableList<String> = mutableListOf(nama,gender,email,alamat,notelepon)
+
+    Column(
+        modifier.fillMaxSize().padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
