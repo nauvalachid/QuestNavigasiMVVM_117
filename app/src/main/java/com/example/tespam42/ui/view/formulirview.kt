932ccdb6a0826a41f4eb543c1aa9f2efad2bb48e
nauvalachid <nauvalachid@gmail.com>
2024-11-21 20:54:52 +0700

    Menambahkan form input dengan `TextField` untuk nama, email, alamat, dan nomor telepon, serta `RadioButton` untuk memilih jenis kelamin. Menyisipkan `Spacer` untuk memberikan jarak antar elemen sehingga tata letak lebih rapi dan responsif.

--- app/src/main/java/com/example/tespam42/ui/view/formulirview.kt
+++ app/src/main/java/com/example/tespam42/ui/view/formulirview.kt
@@ -1,8 +1,15 @@
 package com.example.tespam42.ui.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -27,3 +35,67 @@ fun FormulirView(modifier: Modifier = Modifier, listJK : List<String>,
         modifier.fillMaxSize().padding(30.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        TextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = {
+                Text(text = "nama")
+            },
+            placeholder = {
+                Text(text = "isi nama anda")
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            listJK.forEach { selectedGender ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = gender == selectedGender, onClick = {
+                            gender = selectedGender
+                        })
+                    Text(text = selectedGender)
+                    Spacer(modifier = Modifier.padding(10.dp))
+                }
+            }
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = {
+                Text(text = "email")
+            },
+            placeholder = {
+                Text(text = "isi email anda")
+            },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        TextField(
+            value = alamat,
+            onValueChange = { alamat = it },
+            label = {
+                Text(text = "alamat")
+            },
+            placeholder = {
+                Text(text = "isi alamat anda")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        TextField(
+            value = notelepon,
+            onValueChange = { notelepon = it },
+            label = {
+                Text(text = "notelepon")
+            },
+            placeholder = {
+                Text(text = "isi notelepon anda")
+            },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+    }
