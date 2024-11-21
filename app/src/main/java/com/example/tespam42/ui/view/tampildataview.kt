cc67a3a74c9e982c861fbd40f744e8433488ae04
nauvalachid <nauvalachid@gmail.com>
2024-11-21 21:02:25 +0700

    Menambahkan fungsi `TampilData` untuk menampilkan pasangan parameter dan argumen secara dinamis dalam format baris. Menggunakan fungsi ini di `TampilDataView` untuk menampilkan data siswa seperti nama, jenis kelamin, email, alamat, dan nomor telepon. Juga menambahkan tombol "Kembali" dengan callback navigasi & fiksasi tampildataview.kt

--- app/src/main/java/com/example/tespam42/ui/view/tampildataview.kt
+++ app/src/main/java/com/example/tespam42/ui/view/tampildataview.kt
@@ -1,8 +1,13 @@
 package com.example.tespam42.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,5 +22,40 @@ fun TampilDataView(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
-    ){
+    ){ TampilData(parameterName = "Nama", argu = uiState.nama)
+        TampilData(parameterName = "Jenis Kelamin", argu = uiState.gender)
+        TampilData(parameterName = "Email", argu = uiState.email)
+        TampilData(parameterName = "Alamat", argu = uiState.alamat)
+        TampilData(parameterName = "No Telepon", argu = uiState.notelepon)
+
+        Button(onClick = onBackButton){
+            Text(text="Kembali")
+        }
+    }
+}
+@Composable
+fun TampilData(
+    parameterName: String, argu : String
+) {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = parameterName,
+                modifier = Modifier.weight(0.8f)
+            )
+            Text(
+                text = ":",
+                modifier = Modifier.weight(0.2f)
+            )
+            Text(
+                text = argu,
+                modifier = Modifier.weight(2f)
+            )
+        }
+    }
 }
