4438690c4c6823f2a244f881e77edbba03b4a167
nauvalachid <nauvalachid@gmail.com>
2024-11-21 20:49:50 +0700

    membuat dataclass "datasiswa"

---
+++ app/src/main/java/com/example/tespam42/model/datasiswa.kt
@@ -0,0 +1,9 @@
+package com.example.tespam42.model
+
+data class datasiswa(
+    val nama: String = "",
+    val gender: String = "",
+    val email: String = "",
+    val alamat: String = "",
+    val notelepon: String = "",
+)
