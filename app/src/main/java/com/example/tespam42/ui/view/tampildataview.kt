773130dae4f701b3654d223ce82dd20937635733
nauvalachid <nauvalachid@gmail.com>
2024-11-21 20:58:29 +0700

    Menambahkan fungsi `TampilDataView` untuk menampilkan data siswa berdasarkan `uiState` dan menyediakan tombol kembali dengan callback `onBackButton`.

---
+++ app/src/main/java/com/example/tespam42/ui/view/tampildataview.kt
@@ -0,0 +1,10 @@
+package com.example.tespam42.ui.view
+
+import androidx.compose.runtime.Composable
+import com.example.tespam42.model.datasiswa
+
+@Composable
+fun TampilDataView(
+    uiState: datasiswa,
+    onBackButton: () -> Unit
+) {
