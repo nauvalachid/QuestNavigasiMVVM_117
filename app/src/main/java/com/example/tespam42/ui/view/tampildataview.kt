c3e08d80c41024f8c2c8271cd6b3b1718e3081ff
nauvalachid <nauvalachid@gmail.com>
2024-11-21 21:00:09 +0700

    Mengatur tata letak utama menggunakan `Column` dengan ukuran penuh layar (`fillMaxSize`) dan memberikan padding sebesar 16dp untuk memastikan elemen di dalamnya memiliki ruang yang cukup pada tampildataview.kt

--- app/src/main/java/com/example/tespam42/ui/view/tampildataview.kt
+++ app/src/main/java/com/example/tespam42/ui/view/tampildataview.kt
@@ -1,6 +1,11 @@
 package com.example.tespam42.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.tespam42.model.datasiswa
 
 @Composable
@@ -8,3 +13,9 @@ fun TampilDataView(
     uiState: datasiswa,
     onBackButton: () -> Unit
 ) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+}
